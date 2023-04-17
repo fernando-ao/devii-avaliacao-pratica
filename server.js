d186e0c3612685a4bc8455b1df3742d34cff812c
Fernando Augusto Ortolan <fernando.ao2000@aluno.ifsc.edu.br>
2023-04-17 19:58:44 -0300

    Add file routes.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const server = express();
 const port = 3000;
+const routes = require("./routes");
 
-server.get("/", (req, res) => {
-  res.status(200).send("OK");
-});
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(routes);
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
