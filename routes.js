10fb6ceb2f51a75eecbfd35cc0de49f1f3245b9b
Fernando Augusto Ortolan <fernando.ao2000@aluno.ifsc.edu.br>
2023-04-17 20:16:31 -0300

    Add route with GET method

--- routes.js
+++ routes.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const routes = express.Router();
 
+const products = [
+  {
+    "id": 1,
+    "descricao": "Camiseta",
+    "preco": 29.99,
+    "cores": ["preto", "branco", "cinza"]
+  },
+  {
+    "id": 2,
+    "descricao": "Calça Jeans",
+    "preco": 79.99,
+    "cores": ["azul claro", "azul escuro"]
+  },
+  {
+    "id": 3,
+    "descricao": "Tênis Esportivo",
+    "preco": 149.99,
+    "cores": ["preto", "branco", "cinza", "azul"]
+  },
+  {
+    "id": 4,
+    "descricao": "Jaqueta de Couro",
+    "preco": 99.99,
+    "cores": ["preto", "marrom"]
+  }
+];
+
 routes.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.status(200).send(products);
+});
+
+routes.get("/:id", (req, res) => {
+  for (const item of products) {
+    const id = req.params.id;
+    if (id == item.id) {
+      res.send(item);
+      return;
+    }
+  }
+  res.status(404).send("Product not found");
 });
 
 module.exports = routes;
